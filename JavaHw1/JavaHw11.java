88b05c7fff5bd56b349c7a579a1c2e3803b128c1
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-04 14:06:33 +0300

    first add JavaHw11.java

--- JavaHw1/JavaHw11.java
+++ JavaHw1/JavaHw11.java
@@ -1,3 +1,35 @@
-public class JavaHw11 {
+// Задача 1. Вычислить [n-ое треугольного число](сумма чисел от 1 до n), n! (произведение чисел от 1 до n).
 
+import java.util.Scanner;
+
+public class JavaHw11 {
+    static int factorial(int n){
+        if(n == 0) {
+            return 0;
+        }
+        else if(n == 1){
+            return 1;
+        }
+        else{
+            return n * factorial(n - 1);
+        }
+    }
+    static int triangle(int n){
+        if(n == 0) {
+            return 0;
+        }
+        else if(n == 1){
+            return 1;
+        }
+        else{
+            return n + triangle(n - 1);
+        }
+    }
+    public static void main(String[] args){
+        Scanner numScan = new Scanner(System.in);
+        System.out.print("Введите число, которые будет n-ым треугольным и факториалом: ");
+        int x = numScan.nextInt();
+        numScan.close();
+        System.out.print("Результат расчета: " + triangle(x) + ", " + factorial(x) + ".");
+    }
 }
