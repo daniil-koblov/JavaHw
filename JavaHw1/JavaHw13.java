01c0510590dfc7678048f35d8b7555da2844598e
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-06 17:44:22 +0300

    seventh add JavaHw13.java

--- JavaHw1/JavaHw13.java
+++ JavaHw1/JavaHw13.java
@@ -1,10 +1,8 @@
-
 import java.util.Scanner;
 
 public class JavaHw13 {
     static String menuPrint(){
-        return "Это простой калькулятор.\n"
-                + "Нажмите 1 для сложения,\n"
+        return "Нажмите 1 для сложения,\n"
                 + "Нажмите 2 для вычитания,\n"
                 + "Нажмите 3 для умножения,\n"
                 + "Нажмите 4 для деления,\n"
@@ -28,34 +26,28 @@ public class JavaHw13 {
     }
     public static void main(String[] args){
         Scanner numScan = new Scanner(System.in);
-        System.out.println(menuPrint());
+        System.out.println("Это простой калькулятор.\n" + menuPrint());
         double x = numScan.nextDouble();
-        System.out.println("Введите первое число: ");
-        double n = numScan.nextDouble();
-        System.out.println("Введите второе число: ");
-        double m = numScan.nextDouble();
+        while(x < 1 || x > 5){
+            System.out.println("Такого варианта нет, повторите ввод.\n"
+                    + menuPrint());
+            double r = numScan.nextDouble();
+            x = r;
+        }
         while (x != 5){
+            System.out.println("Введите первое число: ");
+            double n = numScan.nextDouble();
+            System.out.println("Введите второе число: ");
+            double m = numScan.nextDouble();
             while(x == 4 && m == 0){
                 System.out.println("На ноль делить нельзя. Повторите ввод второго числа: ");
                 double q = numScan.nextDouble();
                 m = q;
             }
-            if(x > 0 && x < 5){
-                    System.out.println("Результат вычисления равен: " + calk(x, n, m) + ".");
-                    System.out.println(menuPrint());
-                    double g = numScan.nextDouble();
-                    x = g;
-                }
-            while(x < 1 || x > 5){
-                System.out.println("Такого варианта нет. Повторите ввод,\n"
-                        + "Нажав 1 для сложения,\n"
-                        + "Нажав 2 для вычитания,\n"
-                        + "Нажав 3 для умножения,\n"
-                        + "Нажав 4 для деления,\n"
-                        + "Или нажав 5 для завершения работы.");
-                double r = numScan.nextDouble();
-                x = r;
-            }
+            System.out.println("Результат вычисления равен: " + calk(x, n, m) + ".");
+            System.out.println(menuPrint());
+            double g = numScan.nextDouble();
+            x = g;
         }
         System.out.println("Работа завершена.");
         numScan.close();
