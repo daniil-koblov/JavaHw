84c2c442ee3b81313e65eca0366ef4891fb8a6ba
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-06 17:15:59 +0300

    fifth add JavaHw13.java

--- JavaHw1/JavaHw13.java
+++ JavaHw1/JavaHw13.java
@@ -2,59 +2,44 @@
 import java.util.Scanner;
 
 public class JavaHw13 {
-    static double calk(double n){
+    static String menuPrint(){
+        return "Это простой калькулятор.\n"
+                + "Нажмите 1 для сложения,\n"
+                + "Нажмите 2 для вычитания,\n"
+                + "Нажмите 3 для умножения,\n"
+                + "Нажмите 4 для деления,\n"
+                + "Или нажмите 5 для завершения работы.";
+    }
+    static double calk(double n, double a, double b){
         double c = 0;
         if (n == 1){
-            Scanner numScan = new Scanner(System.in);
-            System.out.print("Введите первое слагаемое: ");
-            double a = numScan.nextDouble();
-            System.out.print("Введите второе слагаемое: ");
-            double b = numScan.nextDouble();
-            numScan.close();
             c = a + b;
         }
         if (n == 2){
-            Scanner numScan = new Scanner(System.in);
-            System.out.print("Введите уменьшаемое: ");
-            double a = numScan.nextDouble();
-            System.out.print("Введите вычитаемое: ");
-            double b = numScan.nextDouble();
-            numScan.close();
             c = a - b;
         }
-        if (n == 3){    
-            Scanner numScan = new Scanner(System.in);      
-            System.out.print("Введите первый множитель: ");
-            double a = numScan.nextDouble();
-            System.out.print("Введите второй множитель: ");
-            double b = numScan.nextDouble();
-            numScan.close();
+        if (n == 3){
             c = a * b;
         }
-        if (n == 4){   
-            Scanner numScan = new Scanner(System.in);        
-            System.out.print("Введите уменьшаемое: ");
-            double a = numScan.nextDouble();
-            System.out.print("Введите вычитаемое: ");
-            double b = numScan.nextDouble();
-            numScan.close();
+        if (n == 4){
             c = a / b;
         }
         return c;
     }
     public static void main(String[] args){
         Scanner numScan = new Scanner(System.in);
-        System.out.println("Это простой калькулятор.\n"
-                        + "Нажмите 1 для сложения,\n"
-                        + "Нажмите 2 для вычитания,\n"
-                        + "Нажмите 3 для умножения,\n"
-                        + "Нажмите 4 для деления,\n"
-                        + "Или нажмите 5 для завершения работы.");
+        System.out.println(menuPrint());
         double x = numScan.nextDouble();
+        System.out.println("Введите первое число: ");
+        double n = numScan.nextDouble();
+        System.out.println("Введите второе число: ");
+        double m = numScan.nextDouble();
         while (x != 5){
             if(x > 0 && x < 5){
-                System.out.println("Результат вычисления равен: " + calk(x) + ".");
-                break;
+                System.out.println("Результат вычисления равен: " + calk(x, n, m) + ".");
+                System.out.println(menuPrint());
+                double g = numScan.nextDouble();
+                x = g;
             }
             while(x < 1 || x > 5){
                 System.out.println("Такого варианта нет. Повторите ввод,\n"
