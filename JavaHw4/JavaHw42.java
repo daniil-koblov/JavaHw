2639f0a5d1ce453d8b4127255404aa041b4569c9
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-19 20:27:45 +0300

    first add JavaHw42.java

--- JavaHw4/JavaHw42.java
+++ JavaHw4/JavaHw42.java
@@ -1,5 +1,44 @@
+// Задача 2. Реализуйте очередь с помощью LinkedList со следующими методами:
+// • enqueue() — помещает элемент в конец очереди,
+// • dequeue() — возвращает первый элемент из очереди и удаляет его,
+// • first() — возвращает первый элемент из очереди, не удаляя.
 package JavaHw4;
 
+import java.util.LinkedList;
+
 public class JavaHw42 {
-    
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            enqueue(ll, getRandomNumber());
+        }
+        System.out.println(ll);
+
+        System.out.println(dequeue(ll));
+        System.out.println(ll);
+
+        System.out.println(first(ll));
+        System.out.println(ll); 
+    }
+
+    public static void enqueue(LinkedList<Integer> ll, int num) {
+        ll.addLast(num);
+    }
+
+    public static int dequeue(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        ll.remove(0);
+        return num;
+    }
+
+    public static int first(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        return num;
+    }
+
+    public static int getRandomNumber() {
+        double x = (Math.random() * 99);
+        int num = (int) x;
+        return num;
+    }
 }
